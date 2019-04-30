2379184754792a8ab986879775b114beac22d3dd
John <jl.allemann@gmail.com>
2019-04-30 13:58:16 +0200

    reorganize

---
+++ src/GatewayException.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace PayXpert\Gateway;
+
+use Exception;
+
+/**
+ * Custom class for the Gateway client Exceptions
+ *
+ * This class doesn't have any specific method or property.
+ * We only use this class for its name, to separate between PHP Exceptions and
+ * our own exception.
+ */
+class GatewayException extends Exception 
+{
+
+}
+
